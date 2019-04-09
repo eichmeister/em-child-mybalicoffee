ad16b0aff08c6da7d3cb317e4599fe5cc34b6470
Maximilian Radmacher <maximilian.radmacher@googlemail.com>
2019-04-09 15:11:07 +0200

    add taxonomys, filters and update product_data

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -9,44 +9,115 @@
 		function em_custom_product_taxonomy()  {
 
 			register_taxonomy(
-				'product_cat_2',
-				apply_filters( 'woocommerce_taxonomy_objects_product_cat', array( 'product' ) ),
-				apply_filters(
-					'woocommerce_taxonomy_args_product_cat', array(
-						'hierarchical'          => true,
-						'update_count_callback' => '_wc_term_recount',
-						'label'                 => __( 'Categories', 'woocommerce' ),
-						'labels'                => array(
-							'name'              => __( 'Product categories', 'woocommerce' ),
-							'singular_name'     => __( 'Category', 'woocommerce' ),
-							'menu_name'         => _x( 'Categories', 'Admin menu name', 'woocommerce' ),
-							'search_items'      => __( 'Search categories', 'woocommerce' ),
-							'all_items'         => __( 'All categories', 'woocommerce' ),
-							'parent_item'       => __( 'Parent category', 'woocommerce' ),
-							'parent_item_colon' => __( 'Parent category:', 'woocommerce' ),
-							'edit_item'         => __( 'Edit category', 'woocommerce' ),
-							'update_item'       => __( 'Update category', 'woocommerce' ),
-							'add_new_item'      => __( 'Add new category', 'woocommerce' ),
-							'new_item_name'     => __( 'New category name', 'woocommerce' ),
-							'not_found'         => __( 'No categories found', 'woocommerce' ),
-						),
-						'show_ui'               => true,
-						'query_var'             => true,
-						'capabilities'          => array(
-							'manage_terms' => 'manage_product_terms',
-							'edit_terms'   => 'edit_product_terms',
-							'delete_terms' => 'delete_product_terms',
-							'assign_terms' => 'assign_product_terms',
-						),
-						'rewrite'               => array(
-							'slug'         => $permalinks['category_rewrite_slug'],
-							'with_front'   => false,
-							'hierarchical' => true,
-						),
-					)
+				'product_flavor',
+				array( 'product' ),
+				array(
+					'hierarchical'          => false,
+					'show_ui'               => true,
+					'query_var'             => true,
+					'parent_item'       	=> null,
+					'parent_item_colon' 	=> null,
+					// 'capabilities'          => array(
+					// 	'manage_terms' => 'manage_product_terms',
+					// 	'edit_terms'   => 'edit_product_terms',
+					// 	'delete_terms' => 'delete_product_terms',
+					// 	'assign_terms' => 'assign_product_terms',
+					// ),
+					// 'rewrite'               => array(
+					// 	'slug'         => $permalinks['category_rewrite_slug'],
+					// 	'with_front'   => false,
+					// 	'hierarchical' => true,
+					// ),
+					'label'                 => __( 'Bean Flavor', 'eichmeister' ),
+					'labels'                => array(
+						'name'              => __( 'Bean Flavor', 'eichmeister' ),
+						'singular_name'     => __( 'Flavor', 'eichmeister' ),
+						'menu_name'         => _x( 'Flavors', 'Admin menu name', 'eichmeister' ),
+						'search_items'      => __( 'Search flavors', 'eichmeister' ),
+						'all_items'         => __( 'All flavors', 'eichmeister' ),
+						'parent_item'       => __( 'Parent flavor', 'eichmeister' ),
+						'parent_item_colon' => __( 'Parent flavor:', 'eichmeister' ),
+						'edit_item'         => __( 'Edit flavor', 'eichmeister' ),
+						'update_item'       => __( 'Update flavor', 'eichmeister' ),
+						'add_new_item'      => __( 'Add new flavor', 'eichmeister' ),
+						'new_item_name'     => __( 'New flavor name', 'eichmeister' ),
+						'not_found'         => __( 'No flavors found', 'eichmeister' ),
+					),
 				)
 			);
 
+			register_taxonomy(
+				'product_region',
+				array( 'product' ),
+				array(
+					'hierarchical'          => false,
+					'show_ui'               => true,
+					'query_var'             => true,
+					'parent_item'       	=> null,
+					'parent_item_colon' 	=> null,
+					// 'capabilities'          => array(
+					// 	'manage_terms' => 'manage_product_terms',
+					// 	'edit_terms'   => 'edit_product_terms',
+					// 	'delete_terms' => 'delete_product_terms',
+					// 	'assign_terms' => 'assign_product_terms',
+					// ),
+					// 'rewrite'               => array(
+					// 	'slug'         => $permalinks['category_rewrite_slug'],
+					// 	'with_front'   => false,
+					// 	'hierarchical' => true,
+					// ),
+					'label'                 => __( 'Region', 'eichmeister' ),
+					'labels'                => array(
+						'name'              => __( 'Region', 'eichmeister' ),
+						'singular_name'     => __( 'Region', 'eichmeister' ),
+						'menu_name'         => _x( 'Regions', 'Admin menu name', 'eichmeister' ),
+						'search_items'      => __( 'Search regions', 'eichmeister' ),
+						'all_items'         => __( 'All regions', 'eichmeister' ),
+						'parent_item'       => __( 'Parent region', 'eichmeister' ),
+						'parent_item_colon' => __( 'Parent region:', 'eichmeister' ),
+						'edit_item'         => __( 'Edit region', 'eichmeister' ),
+						'update_item'       => __( 'Update region', 'eichmeister' ),
+						'add_new_item'      => __( 'Add new region', 'eichmeister' ),
+						'new_item_name'     => __( 'New region name', 'eichmeister' ),
+						'not_found'         => __( 'No regions found', 'eichmeister' ),
+					),
+				)
+			);
 		}
 		add_action( 'woocommerce_after_register_taxonomy', 'em_custom_product_taxonomy' );
 	}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// EDIT EICHMEISTER PRODUCT DATA
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Add custom Fields
+    include( locate_template('plugins/woocommerce/fields/em-wc-product-fields.php') );
+
+    // Remove meta box "eichmeister product data"
+    add_filter( 'em_wc_product_activate', '__return_false' );
+
+ //    if ( ! function_exists( 'update_em_text_box_1' ) ) {
+ //        function update_em_text_box_1( $text ) {
+ //        	$text = 'Geschmack';
+ //            return $text;
+ //        }
+ //        add_filter('em_text_box_1', 'update_em_text_box_1', 10, 4);
+ //    }
+
+ //    if ( ! function_exists( 'update_em_text_box_2' ) ) {
+ //        function update_em_text_box_2( $text ) {
+ //        	$text = 'Herkunft';
+ //            return $text;
+ //        }
+ //        add_filter('em_text_box_2', 'update_em_text_box_2', 10, 4);
+ //    }
+
+ //    if ( ! function_exists( 'update_em_text_box_3' ) ) {
+ //        function update_em_text_box_3( $text ) {
+ //        	$text = 'Lieferung';
+ //            return $text;
+ //        }
+ //        add_filter('em_text_box_3', 'update_em_text_box_3', 10, 4);
+ //    }
+	// 
