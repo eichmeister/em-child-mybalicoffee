ac01b30c28792e6b388bc65d4f654e276c4ae140
Maximilian Radmacher <maximilian.radmacher@googlemail.com>
2019-06-03 17:35:52 +0200

    updated menu order updated page.php for checkout and cart

--- page.php
+++ page.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
+<?php if (is_checkout() || is_cart()) { ?>
+	<div class="wrapper-1200">
+<?php } else { ?>
+	<div class="wrapper-1200 em-txt padding-top-50">
+<?php } ?>
 <!-- Page.php -->
-<div class="wrapper-1200 em-txt padding-top-50">
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
