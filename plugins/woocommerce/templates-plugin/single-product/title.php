f6508771adb1a1ba713d830dfeb0e33471dfb998
Tim-Fabian Köck <koeck@eichmeister.de>
2019-08-02 10:26:49 +0200

    display flavor only in title for coffee template

--- plugins/woocommerce/templates-plugin/single-product/title.php
+++ plugins/woocommerce/templates-plugin/single-product/title.php
@@ -2,6 +2,6 @@
 $region_field = get_field( 'gebiet_region', $product->get_id() );
 $flavor_field = get_field( 'bohnenart', $product->get_id() );
 
-echo '<h2 class="span">' . $flavor_field->name . '</h2>';
+if ( get_field('product_template') == 1 ) echo '<h2 class="span">' . $flavor_field->name . '</h2>';
 the_title( '<h1 class="hl1">', '</h1>' );
 echo '<h3 class="span">' . $region_field->name . '</h3>';
