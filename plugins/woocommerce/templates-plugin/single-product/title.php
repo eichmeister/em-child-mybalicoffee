60c9aaa8c4af74fcd0b1145be5a6adf4840def34
Tim-Fabian Köck <koeck@eichmeister.de>
2019-08-02 15:29:02 +0200

    title adjust

--- plugins/woocommerce/templates-plugin/single-product/title.php
+++ plugins/woocommerce/templates-plugin/single-product/title.php
@@ -4,4 +4,4 @@ $flavor_field = get_field( 'bohnenart', $product->get_id() );
 
 if ( get_field('product_template') == 1 ) echo '<h2 class="span">' . $flavor_field->name . '</h2>';
 the_title( '<h1 class="hl1">', '</h1>' );
-echo '<h3 class="span">' . $region_field->name . '</h3>';
+if ( get_field('product_template') == 1 ) echo '<h3 class="span">' . $region_field->name . '</h3>';
