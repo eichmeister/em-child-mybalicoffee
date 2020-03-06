3a566887133d11ba1e81ce582c1d02a9aeab32ce
Gary McNulty <garymcnulty1@gmail.com>
2020-03-06 09:10:44 +0100

    Team section added to about template

--- page-templates/tpl-about.php
+++ page-templates/tpl-about.php
@@ -47,6 +47,50 @@ Template Name: MyBali - About
 
 </section>
 
+<?php if ( is_user_logged_in() ): ?>
+	<?php if ( current_user_can( 'edit_pages' ) ): ?>
+
+		<section id="team">
+
+			<div class="wrapper-1200">
+				
+				<div class="row">
+					<div class="col_12">
+						<h2 class="hl-4 center">
+							<?php echo first_line_bold( get_field('team_hl') ); ?>
+						</h2>
+					</div>
+				</div>
+				
+				<div class="row teamrow">
+
+					<?php $i = 0; ?>	
+					<?php if ( have_rows('team') ): ?>
+						<?php while (have_rows('team')) : the_row(); ?>
+
+							<div class="col_4">
+
+								<figure <?php EM()->Template->bg_cover(get_sub_field('img'), 'img_800'); ?>></figure>
+
+								<div class="inner">
+                                    <h4 class="hl4"><?php the_sub_field('name'); ?></h4>
+									<p><?php the_sub_field('role'); ?></p>
+								</div>
+
+							</div>
+
+							<?php $i++; ?>
+						<?php endwhile; ?>
+					<?php endif; ?>
+
+				</div>
+			</div>
+
+		</section>
+
+	<?php endif; ?>
+<?php endif; ?>
+
 <section id="contact">
 
 	<div class="wrapper-1200 padding-ver-50">
